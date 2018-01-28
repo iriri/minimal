007988e0a91fce2c0e60e5396946962194f0caf6
iriri <iri@konnichiwastevenspielbergde.su>
2018-01-27 19:48:29 -0800

    Added support for int flags; tfw no generics

--- flag/flag.go
+++ flag/flag.go
@@ -3,7 +3,8 @@ package flag
 import (
 	"fmt"
 	"os"
-	//"strings"
+	"sort"
+	"strconv"
 )
 
 type ftype int
@@ -14,65 +15,46 @@ const (
 	longFlag
 )
 
-type flag interface {
+type flagVal interface {
 	set(interface{})
-	printUsageAndDelete()
 }
 
-type boolFlag struct {
-	val     *bool
-	baseVal bool
-	usage   string
-	long    string
-	short   rune
+type boolVal bool
+type intVal int
+type stringVal string
+
+type flag struct {
+	val   flagVal
+	usage string
+	long  string
+	short rune
 }
 
-type stringFlag struct {
-	val     *string
-	baseVal string
-	usage   string
-	long    string
-	short   rune
+func (v *boolVal) set(b interface{}) {
+	*v = boolVal(b.(bool))
 }
 
-func (f *boolFlag) set(b interface{}) {
-	*f.val = b.(bool)
+func (v *intVal) set(i interface{}) {
+	*v = intVal(i.(int))
 }
 
-func (f *boolFlag) printUsageAndDelete() {
+func (v *stringVal) set(s interface{}) {
+	*v = stringVal(s.(string))
+}
+
+func (f flag) printUsageAndDelete() {
 	if f.short != 0 && f.long != "" {
-		fmt.Fprintf(os.Stderr, "    -%c --%s\t%s [default: %t]\n",
-			f.short, f.long, f.usage, f.baseVal)
+		fmt.Fprintf(os.Stderr, "    -%c --%s\t%s\n",
+			f.short, f.long, f.usage)
 		delete(shortFlags, f.short)
 		delete(longFlags, f.long)
 	} else if f.short != 0 {
-		fmt.Fprintf(os.Stderr, "    -%c\t\t%s [default: %t]\n",
-			f.short, f.usage, f.baseVal)
+		fmt.Fprintf(os.Stderr, "    -%c\t\t%s\n",
+			f.short, f.usage)
 		delete(shortFlags, f.short)
 	} else if f.long != "" {
-		fmt.Fprintf(os.Stderr, "    --%s\t%s [default: %t]\n",
-			f.long, f.usage, f.baseVal)
-		delete(longFlags, f.long)
-	}
-}
-
-func (f *stringFlag) set(s interface{}) {
-	*f.val = s.(string)
-}
-
-func (f *stringFlag) printUsageAndDelete() {
-	if f.short != 0 && f.long != "" {
-		fmt.Fprintf(os.Stderr, "    -%c --%s\t%s [default: %s]\n",
-			f.short, f.long, f.usage, f.baseVal)
-		delete(shortFlags, f.short)
-		delete(longFlags, f.long)
-	} else if f.short != 0 {
-		fmt.Fprintf(os.Stderr, "    -%c\t\t%s [default: %s]\n",
-			f.short, f.usage, f.baseVal)
-		delete(shortFlags, f.short)
-	} else if f.long != "" {
-		fmt.Fprintf(os.Stderr, "    --%s\t%s [default: %s]\n",
-			f.long, f.usage, f.baseVal)
+		fmt.Fprintf(os.Stderr, "    --%s\t%s\n",
+			f.long, f.usage)
 		delete(longFlags, f.long)
 	}
 }
@@ -81,27 +63,36 @@ var shortFlags = make(map[rune]flag)
 var longFlags = make(map[string]flag)
 
 func Bool(val *bool, baseVal bool, usage string, long string, short rune) {
-	f := boolFlag{val, baseVal, usage, long, short}
+	f := flag{(*boolVal)(val), usage, long, short}
 	*val = baseVal
-
 	if short != 0 {
-		shortFlags[short] = &f
+		shortFlags[short] = f
 	}
 	if long != "" {
-		longFlags[long] = &f
+		longFlags[long] = f
+	}
+}
+
+func Int(val *int, baseVal int, usage string, long string, short rune) {
+	f := flag{(*intVal)(val), usage, long, short}
+	*val = baseVal
+	if short != 0 {
+		shortFlags[short] = f
+	}
+	if long != "" {
+		longFlags[long] = f
 	}
 }
 
 func String(val *string, baseVal string, usage string, long string,
 	short rune) {
-	f := stringFlag{val, baseVal, usage, long, short}
+	f := flag{(*stringVal)(val), usage, long, short}
 	*val = baseVal
-
 	if short != 0 {
-		shortFlags[short] = &f
+		shortFlags[short] = f
 	}
 	if long != "" {
-		longFlags[long] = &f
+		longFlags[long] = f
 	}
 }
 
@@ -122,17 +113,32 @@ func parseShortFlag(i int) int {
 			fmt.Fprintf(os.Stderr, "Invalid flag: -%c\n", r)
 			printUsageAndExit()
 		}
-		switch f := f.(type) {
-		case *boolFlag:
-			f.set(!*f.val)
-		case *stringFlag:
-			if j != len(os.Args[i])-1 ||
+		switch t := f.val.(type) {
+		case *boolVal:
+			t.set(!*t)
+		case *intVal:
+			if j != len(os.Args[i])-2 || len(os.Args[i:]) < 2 {
+				fmt.Fprintf(os.Stderr, "%d %d", j, len(os.Args[i:]))
+				fmt.Fprintf(os.Stderr,
+					"-%c must precede integer\n", r)
+				printUsageAndExit()
+			}
+			n, err := strconv.Atoi(os.Args[i+1])
+			if err != nil {
+				fmt.Fprintf(os.Stderr,
+					"-%c must precede integer\n", r)
+				printUsageAndExit()
+			}
+			t.set(n)
+			return 1
+		case *stringVal:
+			if j != len(os.Args[i])-1 || len(os.Args[i:]) < 2 ||
 				isFlag(os.Args[i+1]) != notFlag {
 				fmt.Fprintf(os.Stderr,
 					"-%c must precede string\n", r)
 				printUsageAndExit()
 			}
-			f.set(os.Args[i+1])
+			t.set(os.Args[i+1])
 			return 1
 		default:
 			fmt.Fprintf(os.Stderr, "Invalid flag: -%c\n", r)
@@ -148,16 +154,30 @@ func parseLongFlag(i int) int {
 		fmt.Fprintf(os.Stderr, "Invalid flag: %s\n", os.Args[i])
 		printUsageAndExit()
 	}
-	switch f := f.(type) {
-	case *boolFlag:
-		f.set(!*f.val)
-	case *stringFlag:
-		if isFlag(os.Args[i+1]) != notFlag {
+	switch t := f.val.(type) {
+	case *boolVal:
+		t.set(!*t)
+	case *intVal:
+		if len(os.Args[i:]) < 2 {
+			fmt.Fprintf(os.Stderr,
+				"%c must precede integer\n", os.Args[i])
+			printUsageAndExit()
+		}
+		n, err := strconv.Atoi(os.Args[i+1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr,
+				"%c must precede integer\n", os.Args[i])
+			printUsageAndExit()
+		}
+		t.set(n)
+		return 1
+	case *stringVal:
+		if len(os.Args[i:]) < 2 || isFlag(os.Args[i+1]) != notFlag {
 			fmt.Fprintf(os.Stderr, "%s must precede string\n",
 				os.Args[i])
 			printUsageAndExit()
 		}
-		f.set(os.Args[i+1])
+		t.set(os.Args[i+1])
 		return 1
 	default:
 		fmt.Fprintf(os.Stderr, "Invalid flag: %s\n", os.Args[i])
@@ -183,11 +203,21 @@ func Parse(firstFlag int) int {
 
 func printUsageAndExit() {
 	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
-	for _, f := range shortFlags {
-		f.printUsageAndDelete()
+	shortKeys := make([]int, len(shortFlags))
+	for r := range shortFlags {
+		shortKeys = append(shortKeys, int(r))
 	}
-	for _, f := range longFlags {
-		f.printUsageAndDelete()
+	sort.Ints(shortKeys)
+	for _, r := range shortKeys {
+		shortFlags[rune(r)].printUsageAndDelete()
+	}
+	longKeys := make([]string, len(longFlags))
+	for s := range longFlags {
+		longKeys = append(longKeys, s)
+	}
+	sort.Strings(longKeys)
+	for _, s := range longKeys {
+		longFlags[s].printUsageAndDelete()
 	}
 	os.Exit(0)
 }
